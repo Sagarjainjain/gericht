bb1c54b18d5b86ea041da299a42d4e75eeff5cc5
Sagarjainjain <sagarjain.jain10@gmail.com>
2023-06-19 00:08:50 +0530

    third_changes

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SubHeading, MenuItem } from "../../components";
-import { images, data } from "../../constants";
+import { images } from "../../constants";
+import {wines, cocktails} from "../../constants/data"
 import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
@@ -13,7 +14,7 @@ const SpecialMenu = () => (
       <div className="app__specialMenu-menu_wine flex__center">
         <p className="app__specialMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialMenu-menu_items">
-          {data.wines.map((wine, index) => (
+          {wines.map((wine, index) => (
             <MenuItem
               key={wine.title + index}
               price={wine.price}
@@ -29,7 +30,7 @@ const SpecialMenu = () => (
       <div className="app__specialMenu-menu_cocktails flex__center">
         <p className="app__specialMenu_menu_heading">Cocktails</p>
         <div className="app__specialMenu-menu_items">
-          {data.cocktails.map((cocktail, index) => (
+          {cocktails.map((cocktail, index) => (
             <MenuItem
               key={cocktail.title + index}
               title={cocktail.title}
